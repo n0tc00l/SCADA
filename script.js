30c287bbba3290f7ee763ed1f2f8813daf58019d
n0tc00l <113791763+n0tc00l@users.noreply.github.com>
2023-10-31 13:24:42 +0400

    1111

--- script.js
+++ script.js
@@ -1,67 +1,70 @@
-function generateRandomValues(count) {
-  let data = {};
-  for (let i = 1; i <= count; i++) {
-    let name = `A${i}`;
-    let value = Math.floor(Math.random() * 80);
-    data[name] = value;
-  }
-  return data;
-}
 const variables = { pressure: 0, flow: 0 };
+
 for (let v in variables)
   variables[v] = Math.floor(Math.random() * 80);
 
 function addAttributes(value) {
+
   if (value > 50) {
+
     return 'data-max="50" data-color="red"';
+
   } else if (value <= 30) {
+
     return 'data-max="30" data-color="green"';
+
   } else {
+
     return 'data-max="50" data-color="yellow"';
+
   }
+
 }
 
-function createTable(data) {
-  let table = document.getElementById('myTable');
-  let headerRow = table.insertRow();
-  let nameHeader = headerRow.insertCell();
-  nameHeader.innerHTML = '<b>Name</b>';
-  let valueHeader = headerRow.insertCell();
-  valueHeader.innerHTML = '<b>Value</b>';
+window.onload = function() {
+  for (let v in variables) 
+  {  document.querySelectorAll ('.svg')[0].contentDocument.querySelectorAll(`.${v}`)[0].textContent = variables[v]}
 
-  for (let name in data) {
-    let value = data[name];
-    let row = table.insertRow();
-    let nameCell = row.insertCell();
-    nameCell.innerHTML = name;
-    let valueCell = row.insertCell();
-    valueCell.innerHTML = value;
-    valueCell.setAttribute('data-value', value);
-    let attributes = addAttributes(value);
-    valueCell.setAttribute('data-attributes', attributes);
-  }
 }
+    
 
-let data = generateRandomValues();
-createTable(data);
 
-let valueCells = document.querySelectorAll('td[data-attributes]');
+let valueCells = document.querySelectorAll('[data-attributes]');
+
 valueCells.forEach(cell => {
+
   let attributes = cell.getAttribute('data-attributes');
+
   if (attributes) {
+
     let max = attributes.includes('data-max="50"') || attributes.includes('data-max="30"');
+
     let color = attributes.includes('data-color');
+
     if (max) {
+
       cell.setAttribute('data-max', '50');
+
     }
+
     if (color) {
+
       if (attributes.includes('data-color="red"')) {
+
         cell.style.backgroundColor = 'red';
+
       } else if (attributes.includes('data-color="green"')) {
+
         cell.style.backgroundColor = 'green';
+
       } else {
+
         cell.style.backgroundColor = 'yellow';
+
       }
+
     }
+
   }
+
 });
